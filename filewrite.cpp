c5dfee39a4cbe026d303be62df4d19fd8554b9b6
pattonbrown <2998691695@qq.com>
2023-06-26 21:09:10 +0800

    文件写入操作完成

---
+++ filewrite.cpp
@@ -0,0 +1,12 @@
+#include<head.h>
+bool writeFile(const list<User_login>& userList,const string& fileName){
+    ofstream file(fileName);
+    if(!file.is_open()){
+        cerr<<"Failed to open the file"<<endl;
+        return false;
+    }
+    for(User_login user:userList){
+        file<<user.name<<","<<user.count<<endl;
+    }
+    file.close();
+}
