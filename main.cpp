3db3667737d18fc9cb32596d884fef9d252a0ee7
pattonbrown <2998691695@qq.com>
2023-06-26 21:25:34 +0800

    进一步完善代码

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include<head.h>
+extern bool readFile(list<User_login>& userList,const string& fileName);
+extern void merge(list<User_login>& userList);
+extern bool writeFile(const list<User_login>& userList,const string& fileName);
 int main(int argc, char const *argv[])
 {
     list <User_login> userList;
